bab47266ce19ad4df9d8df44ea5dc320fca359f7
milomilo88 <punkeltje@gmail.com>
2023-12-08 17:09:53 +0900

    made a seperate day_1, now using main as a template

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,12 @@
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs;
 
-fn main() -> std::io::Result<()> {
-    let mut content = File::open("assets/puzzle_input.txt").expect("couldn't find file");
-    let mut buffer = String::new();
-
-    content.read_to_string(&mut buffer)?;
-
-    let lines = buffer.split("\n").collect::<Vec<_>>();
-
-    let just_numbers = lines
-        .iter()
-        .map(|line| {
-            line.to_lowercase()
-                .replace("one", "o1e")
-                .replace("two", "t2o")
-                .replace("three", "t3e")
-                .replace("four", "f4r")
-                .replace("five", "f5e")
-                .replace("six", "s6x")
-                .replace("seven", "s7n")
-                .replace("eight", "e8t")
-                .replace("nine", "n9e")
-                .chars()
-                .filter(|char| "123456789".contains(*char))
-                .map(|char| char.to_digit(10).unwrap())
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
-
-    let mut awnser = 0;
-
-    for numbers in just_numbers {
-        let sum = match numbers.len() {
-            0 => {
-                continue;
-            }
-            1 => numbers[0] * 11,
-            2 => (numbers[0] * 10) + numbers[1],
-            _ => (numbers[0] * 10) + numbers[numbers.len() - 1],
-        };
-        // println!("{sum}");
-        awnser += sum;
-    }
-
-    println!("the anwser = {:?}", awnser);
-
-    Ok(())
+fn main() {
+    //my template for importing the advent puzzles :)
+    let content = match fs::read_to_string("assets/test_input.txt") {
+        Ok(content) => content,
+        Err(e) => {
+            eprintln!("Error reading file: {}", e);
+            return;
+        }
+    };
 }
