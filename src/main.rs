6bcc22bf68867ee53f398216f35977f6653c9d1b
milomilo88 <punkeltje@gmail.com>
2024-01-14 18:33:42 +0900

    solved day 12

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{parser::parse_content, spring::Record};
 
 mod parser;
@@ -8,9 +10,11 @@ fn main() {
     let content = parse_content("puzzle_input");
     let mut count = 0;
 
+    let mut mem = HashMap::new();
+
     for line in content.lines() {
         let spring = Record::new(line);
-        count += spring.possible_combinations();
+        count += spring.possible_combinations(&mut mem);
     }
 
     println!("the sum of possible spring combinations = {count}");
--- src/spring.rs
+++ src/spring.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
+
 use crate::util::*;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Record {
     pub spring_conditions: Vec<SpringCondition>,
     pub damaged_springs: Vec<usize>,
@@ -23,58 +25,104 @@ impl Record {
             }
         }
 
+        let multiplier = 5;
+        spring_conditions = (0..multiplier)
+            .flat_map(|_| {
+                spring_conditions
+                    .iter()
+                    .cloned()
+                    .chain(std::iter::once(SpringCondition::Unknown))
+            })
+            .collect();
+
+        spring_conditions.pop();
+
+        damaged_springs = damaged_springs
+            .iter()
+            .cloned()
+            .cycle()
+            .take(damaged_springs.len() * multiplier)
+            .collect();
+
         Record {
             spring_conditions,
             damaged_springs,
         }
     }
 
-    pub fn possible_combinations(&self) -> usize {
-        if let Some(index) = self
-            .spring_conditions
+    pub fn possible_combinations(&self, mem: &mut HashMap<Record, usize>) -> usize {
+        if let Some(&result) = mem.get(self) {
+            return result;
+        }
+
+        if self.damaged_springs.is_empty() {
+            let value = match self
+                .spring_conditions
+                .iter()
+                .any(|spring| *spring == SpringCondition::Damaged)
+            {
+                true => 0,
+                false => 1,
+            };
+
+            mem.insert(self.clone(), value);
+
+            return value;
+        }
+
+        let needed_space =
+            self.damaged_springs.iter().sum::<usize>() + self.damaged_springs.len() - 1;
+
+        if self.spring_conditions.len() < needed_space {
+            mem.insert(self.clone(), 0);
+
+            return 0;
+        }
+
+        let first = self.spring_conditions[0];
+        if first == SpringCondition::Operational {
+            let new_record = Record {
+                spring_conditions: self.spring_conditions[1..].to_vec(),
+                damaged_springs: self.damaged_springs.clone(),
+            };
+
+            let result = new_record.possible_combinations(mem);
+
+            mem.insert(self.clone(), result);
+
+            return result;
+        }
+
+        let group = self.damaged_springs[0];
+        let are_all_non_operational = self.spring_conditions[..group]
             .iter()
-            .position(|spring| *spring == SpringCondition::Unknown)
+            .all(|spring| *spring != SpringCondition::Operational);
+        let end = (group + 1).min(self.spring_conditions.len());
+
+        let mut solutions: usize = 0;
+
+        if are_all_non_operational
+            && ((self.spring_conditions.len() > group
+                && self.spring_conditions[group] != SpringCondition::Damaged)
+                || self.spring_conditions.len() <= group)
         {
-            let mut as_damaged_spring = self.spring_conditions.clone();
-            as_damaged_spring[index] = SpringCondition::Damaged;
-            let as_damaged = Record {
-                spring_conditions: as_damaged_spring,
-                damaged_springs: self.damaged_springs.to_vec(),
+            let new_record = Record {
+                spring_conditions: self.spring_conditions[end..].to_vec(),
+                damaged_springs: self.damaged_springs[1..].to_vec(),
             };
-
-            let mut as_operational_spring = self.spring_conditions.clone();
-            as_operational_spring[index] = SpringCondition::Operational;
-            let as_operational = Record {
-                spring_conditions: as_operational_spring,
-                damaged_springs: self.damaged_springs.to_vec(),
-            };
-
-            as_damaged.possible_combinations() + as_operational.possible_combinations()
-        } else {
-            if self.is_valid() {
-                1
-            } else {
-                0
-            }
-        }
-    }
-
-    fn is_valid(&self) -> bool {
-        let mut iter = self.spring_conditions.iter().peekable();
-        let mut group_counts = Vec::new();
-
-        while let Some(_) = iter.peek() {
-            let count = iter
-                .clone()
-                .take_while(|&&x| x == SpringCondition::Damaged)
-                .count();
-            iter.nth(count);
-
-            group_counts.push(count);
+            solutions += new_record.possible_combinations(mem);
         }
 
-        group_counts.retain(|&x| x > 0);
+        if first == SpringCondition::Unknown {
+            let new_record = Record {
+                spring_conditions: self.spring_conditions[1..].to_vec(),
+                damaged_springs: self.damaged_springs.clone(),
+            };
+            solutions += new_record.possible_combinations(mem);
+        }
 
-        group_counts == self.damaged_springs
+        mem.insert(self.clone(), solutions);
+
+        solutions
     }
 }
